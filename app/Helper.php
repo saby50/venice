9bbb4d57d7926a408da70f1040ece42b2e137e52
Gurpreet Singh <gurpreet.singh@seekerstech.com>
2020-02-26 11:46:56 +0530

    Upload vendor

--- app/Helper.php
+++ app/Helper.php
@@ -55,10 +55,10 @@ class Helper
   }
     public static function get_item_array($itemids) {
     $itemsarray = array();   
-    list($item_id, $quantity) = explode(",", $itemids);
-    foreach ($item_id as $k => $v) {
+    $item_ids = explode(",", $itemids);
+    foreach ($item_ids as $k => $v) {
       $item_name = Helper::get_menu_item_name($v); 
-      $itemsarray[] = array('item_id' => $v,'item_name' => $item_name,'price' => "100", 'quantity' => $quantity);
+      $itemsarray[] = array('item_id' => $v,'item_name' => $item_name,'price' => "100", 'quantity' => "1");
     }
     return $itemsarray;
   }
@@ -106,7 +106,7 @@ class Helper
       $itemdetails = "";
       $itemids = "";
       foreach ($cart as $key => $value) {
-          $itemids.= $value['item_id']."_".$value['quantity'].",";
+          $itemids.= $value['item_id'].",";
       }
       foreach ($cart as $key => $value) {
         $unit_id = $value['unit_id'];
