c1abc9c28b00957211d745db284c752ef5191797
Gurpreet Singh <gurpreet.singh@seekerstech.com>
2020-03-07 16:11:18 +0530

    Upload vendor

--- resources/views/layouts/headerpwa.blade.php
+++ resources/views/layouts/headerpwa.blade.php
@@ -317,7 +317,7 @@ $(document).ready(function() {
     width: 100%;
     height: 65px;
     position: fixed;
-    top: 59px;
+    top: 0px;
     z-index: 9;
     background: #fff;
     padding: 10px;
