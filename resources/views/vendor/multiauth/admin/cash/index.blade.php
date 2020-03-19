40268d3b4132b43bbec06f61242b3b0d550d8c23
Gurpreet Singh <gurpreet.singh@seekerstech.com>
2020-03-19 13:19:01 +0530

    Upload vendor

--- resources/views/vendor/multiauth/admin/cash/index.blade.php
+++ resources/views/vendor/multiauth/admin/cash/index.blade.php
@@ -227,7 +227,7 @@ $seconds = time();
                 <?php foreach ($events as $key => $value): ?>
                 
                   <tr style="border-bottom: solid 1px #ccc;">
-                    <td><input type="checkbox" name="serviceid[]" class="serviceid" value="<?= "e_".$value->id ?>" data="<?= 'e_'.$i ?>">&nbsp;&nbsp; <strong><?= $value->event_name ?></strong><br />  <br />  
+                    <td><input type="checkbox" name="serviceid[]" class="serviceid" value="<?= "e_".$value->id ?>" data="<?= 'e_'.$i ?>" data-date="<?= $value->event_date ?>" data-time="<?= $value->start_time ?>">&nbsp;&nbsp; <strong><?= $value->event_name ?></strong><br />  <br />  
                      </td>
                        <td style="text-align: center;width: 200px;padding-top: 20px;"><div class="input-group">
                                 <span class="input-group-btn">
@@ -248,7 +248,7 @@ $seconds = time();
                 </div>
 
                 
-            
+                       
                         <span class="price_total datappe_<?= $value->id ?>" id="<?= "e_".$i ?>"></span>
                         <input type="hidden" name="type[]" value="events" class="service_type datate_<?= $value->id ?>" id="<?= "typee_".$i ?>">
                         <input type="hidden" name="serviceid1[]" value="<?= "e_".$value->id ?>">
